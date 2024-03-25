5a4717626585ee5f7acfe1dc23def38af926760f
ClondonoM <c.londono.m@gmail.com>
2024-03-25 18:19:27 -0500

    styling navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/footer/Footer'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="container">
         <Navbar/>
                 {children}
-                
-              <Footer/>  
+                <Footer/>  
+        </div>
+        
               </body>
     </html>
   )
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import styles from "./links.module.css"
+import NavLink from "./navLink/navLink"
 
 const Links = () => {
 
@@ -19,9 +21,9 @@ path:"/blog",
 ]
 
     return (
-        <div>
+        <div className={styles.links}>
 {links.map((link=>(
-    <Link href={link.path} hey={link.title}>{link.title}</Link>
+    <NavLink item={link} key={link.title}/>
 )))}
 
         </div>
