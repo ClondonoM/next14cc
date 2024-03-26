361d6a1a07e8245c7b2349134a88e67412da801c
ClondonoM <c.londono.m@gmail.com>
2024-03-25 21:42:40 -0500

    working in links and lateral menu

--- src/components/navbar/links/navLink/navLink.jsx
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 import styles from './navLink.module.css'
 import { usePathname } from 'next/navigation'
 
-const NavLink = ({item}) => {
+const NavLink = ({ item }) => {
 
-const pathName = usePathname();
+    const pathName = usePathname();
 
     return (
         <Link href={item.path} className={`${styles.container} ${pathName === item.path && styles.active}`}>{item.title}</Link>
